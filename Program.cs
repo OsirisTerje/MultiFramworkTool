a1c34e61ccfd56ccdaf2b3e66a14bd0f06c6a18d
Terje Sandstrom <terje@hermit.no>
2024-10-13 20:37:15 +0200

    Initial code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #if NET8_0
+            Console.WriteLine("Hi, I am running with version .NET 8.0");
+        #elif NET6_0
+            Console.WriteLine("Hi, I am running with version .NET 6.0");
+        #else
+            Console.WriteLine("Hi, I am running with an unknown version.");
+        #endif
+    }
+}
