403ef448c6ac573c424b511a02eeb60e091f5450
Terje Sandstrom <terje@hermit.no>
2024-10-13 22:08:59 +0200

    Updated

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿class Program
+﻿using System.Runtime.InteropServices;
+
+class Program
 {
     static void Main(string[] args)
     {
         #if NET8_0
-            Console.WriteLine("Hi, I am running with version .NET 8.0");
+            Console.WriteLine("Hi, I am running with version .NET 8.0 code");
         #elif NET6_0
-            Console.WriteLine("Hi, I am running with version .NET 6.0");
+            Console.WriteLine("Hi, I am running with version .NET 6.0 code");
         #else
             Console.WriteLine("Hi, I am running with an unknown version.");
         #endif
+                // Get the .NET runtime version
+        string runtimeVersion = RuntimeInformation.FrameworkDescription;
+
+        // Output the version
+        Console.WriteLine($"Hi, I am running with runtimeversion: {runtimeVersion}");
     }
 }
